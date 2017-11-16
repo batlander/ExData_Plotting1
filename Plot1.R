1f1596c6495019998ad2921ff3ab6768be36b8f0
batlander <32139068+batlander@users.noreply.github.com>
2017-11-16 19:27:17 +0100

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -20,5 +20,4 @@ electroplot <-electro[electro$Date %in% c("1/2/2007","2/2/2007") ,]
  #  Plot
 png("plot1.png", width=480, height=480)
 hist(electroplot$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
-dev.off()
-                      
+dev.off()                
