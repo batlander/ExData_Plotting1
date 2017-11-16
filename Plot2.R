b515c7bfc8b65c5308b9eee7077d0210b07fc3c1
batlander <32139068+batlander@users.noreply.github.com>
2017-11-16 19:27:07 +0100

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,11 +1,9 @@
-library(dplyr)
-
-# Read archive .txt
+  # Read archive .txt
 
 electro <- read.csv("household_power_consumption.txt", header = TRUE, 
                     sep = ';', nrows = 70000, dec = ".", stringsAsFactors = FALSE)
 
-# Change factors by numerics
+  # Change factors by numerics
 
 electro$Global_active_power <- as.numeric(electro$Global_active_power)
 electro$Global_reactive_power <- as.numeric(electro$Global_reactive_power)
@@ -15,7 +13,7 @@ electro$Sub_metering_1 <- as.numeric(electro$Sub_metering_1)
 electro$Sub_metering_2 <- as.numeric(electro$Sub_metering_2)
 electro$Sub_metering_2 <- as.numeric(electro$Sub_metering_3)
 
-# Select dates
+  # Select dates
 
 electroplot <-electro[electro$Date %in% c("1/2/2007","2/2/2007") ,]
 
@@ -24,4 +22,3 @@ datetime <- strptime(paste(electroplot$Date, electroplot$Time, sep=" "), "%d/%m/
 png("plot2.png", width=480, height=480)
 plot(datetime, electroplot$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
-
